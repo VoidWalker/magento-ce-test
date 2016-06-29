56b5463db345fef5acb3d749cb48d4e9ca3f4915
a.sohan <a.sohan@ism-ukraine.com>
2016-06-29 14:46:47 +0300

    FE

--- app/code/community/Ism/News/controllers/IndexController.php
+++ app/code/community/Ism/News/controllers/IndexController.php
@@ -5,7 +5,7 @@ class Ism_News_IndexController extends Mage_Core_Controller_Front_Action
     public function indexAction()
     {
         $this->loadLayout();
-        $posts = Mage::getModel('news/news')->getCollection();
+//        $posts = Mage::getModel('news/news')->getCollection();
 //        foreach ($posts as $post) {
 //            echo '<h3>' . $post->getTitle() . '</h3>';
 //            echo nl2br($post->getContent());
