8ea05d4f31ea74b84afaf3921871489f6a5d973d
a.sohan <a.sohan@ism-ukraine.com>
2016-07-18 14:02:25 +0300

    c3.3-Delete-action-on-grid Added method for mass delete

--- app/code/community/Ism/News/controllers/Manage/PostController.php
+++ app/code/community/Ism/News/controllers/Manage/PostController.php
@@ -26,6 +26,29 @@ class Ism_News_Manage_PostController extends Mage_Adminhtml_Controller_Action
         $this->renderLayout();
     }
 
+    public function massDeleteAction()
+    {
+        $postsIds = $this->getRequest()->getParam('news');
+        if (!is_array($postsIds)) {
+            Mage::getSingleton('adminhtml/session')->addError(Mage::helper('adminhtml')->__('Please select post(s)'));
+        } else {
+            try {
+                foreach ($postsIds as $postId) {
+                    $model = Mage::getModel('news/news')->load($postId);
+                    $model->delete();
+                }
+                Mage::getSingleton('adminhtml/session')->addSuccess(
+                    Mage::helper('adminhtml')->__(
+                        'Total of %d record(s) were successfully deleted', count($postsIds)
+                    )
+                );
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            }
+        }
+        $this->_redirect('*/*/index');
+    }
+
     public function deleteAction()
     {
         if ($this->getRequest()->getParam('id') > 0) {
