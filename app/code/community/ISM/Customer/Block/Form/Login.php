51a66441a4d9b5218847325f53e69763c2d78493
a.sohan <a.sohan@ism-ukraine.com>
2016-07-25 15:01:46 +0300

    Sandbox Override block example

---
+++ app/code/community/ISM/Customer/Block/Form/Login.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Class ISM_Customer_Block_Form_Login
+ */
+class ISM_Customer_Block_Form_Login extends Mage_Customer_Block_Form_Login
+{
+    protected function _prepareLayout()
+    {
+        return Mage_Core_Block_Template::_prepareLayout();
+    }
+}
